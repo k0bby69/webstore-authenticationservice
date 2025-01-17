4f36948b8c9e05b3303bdfa30332d9f9a5625fac
k0bby69 <129067381+k0bby69@users.noreply.github.com>
2025-01-17 11:06:16 +0000

    Update auth.js

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -1,10 +1,18 @@
 const { ValidateSignature } = require("../../utils");
 const Customer = require("../../database/models/User");
-module.exports = async (req, res, next) => {
-  const isAuthorized = await ValidateSignature(req);
 
-  if (isAuthorized) {
-    return next();
+module.exports = async (req, res, next) => {
+  try {
+    const isAuthorized = await ValidateSignature(req);
+
+    if (isAuthorized) {
+      return next();
+    }
+
+    return res.status(403).json({ message: "Not Authorized" });
+  } catch (error) {
+    console.error("Error in auth middleware:", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
-  return res.status(403).json({ message: "Not Authorized" });
 };
+
