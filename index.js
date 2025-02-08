7821c07a0edf7afbb78a3a1b51fbab463338d203
k0bby69 <129067381+k0bby69@users.noreply.github.com>
2025-02-08 20:48:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,35 +5,18 @@ const userRoutes = require("./api/user");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const print = console.log;
-const port = process.env.PORT || 8001;
-dotenv.config(); // Load environment variables from .env file
+const port = process.env.PORT || 8001
+dotenv.config();
 
 const app = express();
-
-// CORS configuration to handle preflight and allow only specific origins
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     // Define allowed origins
-//     const allowedOrigins = [
-//       'https://webstore-frontend-9669.onrender.com', // Production Frontend URL
-//       'http://localhost:3000' // Localhost URL (if running locally)
-//     ];
-
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-};
-
-// Use CORS middleware with specific options
-app.use(cors(corsOptions));
-
 app.use(express.json());
+// app.use(cors({
+//   origin: ['http://localhost:3000', 'https://multivendorplatform-shopping-frontend.onrender.com'],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+app.use(cors())
 app.use(express.static(__dirname + "/public"));
 
 async function startApp() {
